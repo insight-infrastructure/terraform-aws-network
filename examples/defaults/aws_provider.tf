e0fd8805a257407812d9496a48f1f6465ff07f5f
rob <robcannonxyz@gmail.com>
2020-05-30 12:49:03 -0700

    adding test and example

--- examples/defaults/aws_provider.tf
+++ examples/defaults/aws_provider.tf
@@ -1,7 +1,7 @@
-variable "region" {
+variable "aws_region" {
   default = "us-east-1"
 }
 
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,4 +1,9 @@
 
+resource "random_pet" "this" {
+  length = 2
+}
+
 module "defaults" {
   source = "../.."
+  id     = random_pet.this.id
 }

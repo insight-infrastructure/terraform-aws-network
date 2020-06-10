ddc1e97529957013fbe95731420de7db39a9c0f9
rob <robcannonxyz@gmail.com>
2020-06-09 21:58:31 -0700

    moving rules to own sg resource

--- sg-prometheus.tf
+++ sg-prometheus.tf
@@ -13,22 +13,6 @@ resource "aws_security_group" "prometheus" {
 
   vpc_id = module.vpc.vpc_id
 
-  dynamic "ingress" {
-    for_each = [
-      9100, # node exporter
-      9113, # nginx exporter - TODO: Needs nginx.conf overview
-      9115, # blackbox exporter
-      8080, # cadvisor
-    ]
-
-    content {
-      from_port = ingress.value
-      to_port   = ingress.value
-      protocol  = "tcp"
-      self      = true
-    }
-  }
-
   egress {
     from_port = 0
     to_port   = 0
@@ -36,4 +20,27 @@ resource "aws_security_group" "prometheus" {
     cidr_blocks = [
     "0.0.0.0/0"]
   }
+}
+
+locals {
+  prometheus_exporter_ports = [
+    9100, # node exporter
+    9113, # nginx exporter - TODO: Needs nginx.conf overview
+    9115, # blackbox exporter
+    8080, # cadvisor
+  ]
+}
+
+resource "aws_security_group_rule" "prometheus_exporter_rules" {
+  count = var.create ? length(local.prometheus_exporter_ports) : 0
+
+  security_group_id = join("", aws_security_group.public.*.id)
+  type              = "ingress"
+
+  self        = true
+  protocol    = "tcp"
+  description = "prometheus exporter port for rule for port ${local.prometheus_exporter_ports[count.index]}"
+
+  from_port = local.prometheus_exporter_ports[count.index]
+  to_port   = local.prometheus_exporter_ports[count.index]
 }
